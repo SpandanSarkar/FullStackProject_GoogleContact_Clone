df0aeaa5fd37c6ff40c850b9128f55be90e2900e
Spandan Sarkar <[spandansarkar71@gmail.com]>
2022-07-28 18:40:25 +0600

    modified after shafe vai

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 const express = require('express')
 require('./db/connection')
+const cors = require('cors');
+
 
 const useRouter = require('./router/user')
 const contactsRouter = require('./router/contacts')
 
-const app = express()
-const port = process.env.PORT || 3000
+const corsOptions ={
+    origin:'http://localhost:4200', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+// var corsOptions = {
+//     origin: ["http://10.11.201.69:4200", "http://10.11.201.92:4200", "http://10.11.200.85:4200", "http://10.11.201.69:4325"],
+//     credentials: true,
+  
+//   };
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+
+const port = process.env.PORT || 4000
 
 // parsing incoming JSON
 // this one line, it's going to automatically parse incoming JSON to an object so that we can access it in our request handlers 
-app.use(express.json())
+
 app.use(useRouter)
 app.use(contactsRouter)
 
