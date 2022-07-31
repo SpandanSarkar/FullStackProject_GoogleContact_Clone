a236131be72d9b2c6bdb9594959e3915cca1cc7b
Spandan Sarkar <[spandansarkar71@gmail.com]>
2022-07-31 11:56:45 +0600

    CROD issue checking

--- src/index.js
+++ src/index.js
@@ -6,22 +6,47 @@ const cors = require('cors');
 const useRouter = require('./router/user')
 const contactsRouter = require('./router/contacts')
 
-const corsOptions ={
-    origin:'http://localhost:4200', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+// const corsOptions ={
+//     origin:'http://localhost:4200', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
 
 // var corsOptions = {
 //     origin: ["http://10.11.201.69:4200", "http://10.11.201.92:4200", "http://10.11.200.85:4200", "http://10.11.201.69:4325"],
 //     credentials: true,
   
-//   };
+//   };asddas
 
 const app = express();
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 
+// app.use(cors({
+//     origin: "https://127.0.0.1:4200"
+// }))
+
+// tried from stackoverflow
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', "https://127.0.0.1:4200");
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+// 
 const port = process.env.PORT || 4000
 
 // parsing incoming JSON
